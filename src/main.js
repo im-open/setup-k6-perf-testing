e0b09d07c623582dbafe36015fbb27ed9b8756bf
Josh Clifford <josh.clifford@extendhealth.com>
2022-06-07 12:55:46 -0600

    Trying out token env.  SRE-233

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ async function run() {
     let version = process.env.VERSION;
     let osArchitecture = process.env.ARCH;
     let extensionDownloadUrl = process.env.EXTENSION_DOWNLOAD_URL;
-    let token = core.getInput('extension-download-token');
+    // let token = core.getInput('extension-download-token');
+    let token = process.env.EXTENSION_DOWNLOAD_TOKEN;
 
     await installer.getK6(version, osArchitecture, extensionDownloadUrl, token);
   } catch (error) {
