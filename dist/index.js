621f17f95f872b18c8c9f3d077811d1fcf23c6d0
Josh Clifford <josh.clifford@extendhealth.com>
2022-06-07 12:56:29 -0600

    Building again.  SRE-233

--- dist/index.js
+++ dist/index.js
@@ -4677,7 +4677,7 @@ async function run() {
     let version = process.env.VERSION;
     let osArchitecture = process.env.ARCH;
     let extensionDownloadUrl = process.env.EXTENSION_DOWNLOAD_URL;
-    let token = core2.getInput('extension-download-token');
+    let token = process.env.EXTENSION_DOWNLOAD_TOKEN;
     await installer.getK6(version, osArchitecture, extensionDownloadUrl, token);
   } catch (error) {
     core2.setFailed(error.message);
